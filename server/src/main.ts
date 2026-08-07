3ec00fde2addd1323e2a652a69bdf91628ae2ee1
miniBear <ax_zaa@windowslive.com>
2026-08-07 19:01:51 +0700

    Fix: enable graceful shutdown on SIGTERM

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,6 +6,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    app.enableShutdownHooks();
     const config = app.get(ConfigService);
 
     app.use(json({ limit: '50mb' }));
